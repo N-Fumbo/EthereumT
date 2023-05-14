d37438d97537d87238137159792ce3b064e422ce
Alexander <74.71.spartacus@gmail.com>
2023-05-15 00:03:11 +0300

    Установка пакета для мапинга. Создан класс для возврата кошелька(WalletDto). Создан класс мапинга между WalletDto и Wallet

---
+++ Services/EthereumT.Api/Controllers/Models/Dto/WalletDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EthereumT.Api.Controllers.Models.Dto
+{
+    public class WalletDto
+    {
+        public int Id { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
---
+++ Services/EthereumT.Api/Controllers/Models/Mapping/WalletProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EthereumT.Api.Controllers.Models.Dto;
+using EthereumT.Domain.Entities;
+
+namespace EthereumT.Api.Controllers.Models.Mapping
+{
+    public class WalletProfile : Profile
+    {
+        public WalletProfile()
+        {
+            CreateMap<Wallet, WalletDto>();
+        }
+    }
+}
