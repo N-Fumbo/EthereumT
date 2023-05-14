5c6f5bc6089ba5f79c9e7e23844d2d41e52df5fd
Alexander <74.71.spartacus@gmail.com>
2023-05-14 23:45:06 +0300

    Определение интерфейсов репозитория и страницы бд.

---
+++ Common/EthereumT.Domain.Base/Repositories/Base/IPage.cs
@@ -0,0 +1,15 @@
+﻿namespace EthereumT.Domain.Base.Repositories.Base
+{
+    public interface IPage<out T>
+    {
+        IEnumerable<T> Items { get; }
+
+        int TotalCount { get; }
+
+        int PageIndex { get; }
+
+        int PageSize { get; }
+
+        int TotalPagesCount => (int)Math.Ceiling((double)TotalCount / PageSize);
+    }
+}
---
+++ Common/EthereumT.Domain.Base/Repositories/Base/IRepositoryAsync.cs
@@ -0,0 +1,9 @@
+﻿using EthereumT.Domain.Base.Entities.Base;
+
+namespace EthereumT.Domain.Base.Repositories.Base
+{
+    public interface IRepositoryAsync<T> where T : IEntity
+    {
+        Task<IPage<T>> GetPageAsync(int pageIndex, int pageSize, CancellationToken cancel = default);
+    }
+}
