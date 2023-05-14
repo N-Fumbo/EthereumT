468ace84e76bdb5c336644912d76002eedcf696d
Alexander <74.71.spartacus@gmail.com>
2023-05-14 23:31:05 +0300

    Подключение базы данных. Создание первой миграции.

---
+++ DAL/EthereumT.PgSQL/Migrations/20230514203032_Init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EthereumT.PgSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Wallets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Address = table.Column<string>(type: "character varying(42)", maxLength: 42, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wallets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wallets");
+        }
+    }
+}
--- Services/EthereumT.Api/Program.cs
+++ Services/EthereumT.Api/Program.cs
@@ -1,4 +1,12 @@
+using EthereumT.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("ApiConntection")
+     ?? throw new InvalidOperationException("Connection string 'ApiConntection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString, m => m.MigrationsAssembly("EthereumT.PgSQL")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
